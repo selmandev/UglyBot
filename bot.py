f0fe9cb74b4ad6a8aaa42c51ed29241c73d7c157
selmandev <118924192+selmandev@users.noreply.github.com>
2024-11-27 20:40:46 +0300

    Update bot.py
    added commands funfacts_all, usefulwebsites, info

--- bot.py
+++ bot.py
@@ -6,13 +6,13 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-cleanlist = [
+clean_list = [
     "Kağıt: 2 ila 6 hafta. Gazete veya kağıt mendil gibi ince kağıtlar daha hızlı, karton gibi daha kalın kağıtlar daha yavaş ayrışır.",
     "Pamuklu kumaş: 1 ila 5 ay. Doğal malzeme olduğu için pamuklu kumaşlar hızlıca ayrışır. Sentetik karışımlar ise çok daha uzun sürer.",
     "Ahşap: 1 ila 3 yılİşlenmemiş, doğal ahşap daha hızlı ayrışır. Vernikli veya boyalı ahşap çok daha uzun süre dayanabilir.",
@@ -25,10 +25,41 @@ cleanlist = [
     "Konserve kutusu: 50 yıl. Paslanabilir ancak tamamen yok olması uzun sürer."
     ]
 
+website_list = [
+    "https://www.worldwildlife.org/",
+    "https://www.nationalgeographic.com/environment/",
+    "https://www.unep.org/",
+    "https://www.ewg.org/",
+    "https://www.nature.org/",
+    "https://www.ecowatch.com/",
+    "https://www.greenpeace.org/",
+    "https://www.drawdown.org/",
+    "https://earthwatch.org/",
+    "https://www.rainforest-alliance.org/",
+    "https://www.treehugger.com/",
+    "https://www.footprintnetwork.org/",
+    "https://350.org/",
+    "https://www.epa.gov/",
+    "https://www.rspb.org.uk/"
+    ]
+
+
 @bot.command()
 async def funfacts(ctx):
-    pull_from_list = random.choice(cleanlist)
+    pull_from_list = random.choice(clean_list)
     await ctx.send(pull_from_list)
+@bot.command()
+async def funfacts_all(ctx):
+    await ctx.send(clean_list)
+@bot.command()
+async def usefulwebsites(ctx):
+    pull_from_weblist = random.choice(website_list)
+    await ctx.send(pull_from_weblist)
+@bot.command()
+async def info(ctx):
+    await ctx.send("/funfacts, /funfacts_all, /usefulwebsites")
+
+
 
 
 bot.run("token")
